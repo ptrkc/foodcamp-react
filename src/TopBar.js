b441a528f4dea5f0f2fee19ba3c997941451c93b
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-05-05 01:56:05 -0300

    Added TopBar structure and some style

---
+++ src/TopBar.js
@@ -0,0 +1,8 @@
+export default function TopBar() {
+    return (
+        <div id="top-bar">
+            FoodCamp
+            <p>Sua comida em 6 minutos</p>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import TopBar from "./TopBar.js";
 
 function App() {
     return (
         <>
-            {/* <TopBar/> */}
+            <TopBar />
             {/* <Content/> */}
             {/* <BottomBar/> */}
         </>
