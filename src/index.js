13168c4efcd93a872ea15b489dd50418aee29554
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-05-05 01:21:39 -0300

    Added images, started css and index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <>
+            {/* <TopBar/> */}
+            {/* <Content/> */}
+            {/* <BottomBar/> */}
+        </>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
