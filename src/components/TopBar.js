165b41ad233aa4c5caccae130d11cf2ec87b6199
thiagoribb <thiribeiro142@gmail.com>
2021-05-11 17:12:03 -0300

    Preparando review

--- src/components/TopBar.js
+++
@@ -1,8 +0,0 @@
-export default function TopBar() {
-    return (
-        <div id="top-bar">
-            <h1>FoodCamp</h1>
-            <p>Sua comida em 6 minutos</p>
-        </div>
-    );
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./css/reset.css";
-import "./css/styles.css";
-
-ReactDOM.render(<App />, document.querySelector(".root"));
